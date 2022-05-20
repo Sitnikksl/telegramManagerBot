bc5db181958709fc6b3ce50f6198eae2d6db1e62
Sitnik Alexander <sitnik.ksl@gmail.com>
2022-05-20 22:31:00 +0700

    node init

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-var temp = 32;
-var main = 32;
+const TelegramApi = require('node-telegram-bot-api')
+const token = '5382418564:AAFTOBZyq4UFUCpwBRPVGxrxdkOFeOvEcKE'
+
+const bot = new TelegramApi(token, {polling: true})
+ bot.on('message', msg=>{
+     console.log(msg)
+ })
